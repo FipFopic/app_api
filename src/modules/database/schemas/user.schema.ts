cf42271dd36d62741364f340652439fc10dcbd3f
Орешко Дмитрий <oreshkod@comfortsoft.ru>
2022-10-15 12:15:35 +0300

    [feat] Add UserSchema

---
+++ src/modules/database/schemas/user.schema.ts
@@ -0,0 +1,48 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { Document } from 'mongoose'
+
+export enum UserStatus {
+  ACTIVE = 'ACTIVE',
+  FROZEN = 'FROZEN',
+  BLOCKED = 'BLOCKED'
+}
+
+@Schema()
+export class User {
+  @Prop({
+    name: 'email',
+    required: true,
+    unique: true,
+    trim: true
+  })
+  email: string
+
+  @Prop({
+    name: 'password',
+    required: true,
+    trim: true
+  })
+  password: string
+
+  @Prop({
+    name: 'is_confirmed',
+    default: false
+  })
+  isConfirmed: boolean
+
+  @Prop({
+    name: 'is_temp_password',
+    default: false
+  })
+  isTempPassword: boolean
+
+  @Prop({
+    name: 'status',
+    enum: UserStatus,
+    default: UserStatus.ACTIVE
+  })
+  status: UserStatus
+}
+
+export type UserDocument = User & Document
+export const UserSchema = SchemaFactory.createForClass(User)
